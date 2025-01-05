df4bd28395494e77d130816b98798ed88a2461cb
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-01-05 16:38:09 +0300

    flash messages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
+import flash  from 'connect-flash';
 import pageRoute from './routes/pageRoute.js';
 import courseRoute from './routes/courseRoute.js';
 import categoryRoute from './routes/categoryRoute.js';
@@ -32,6 +33,11 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.DB_URI }),
   })
 );
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
 
 // routes
 app.use('*', (req, res, next) => {
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+import nodemailer from "nodemailer";
+
 // index
 const getIndexPage = (req, res) => {
   res.status(200).render('index', {
@@ -33,5 +35,51 @@ const getContactPage = (req, res) => {
   });
 };
 
+const sendEmail = async (req, res) => {
 
-export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getContactPage };
+try {
+  const outputMessage = `
+  <h1>Mail Details</h1>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email} </li>
+  </ul>
+  <h1>Message</h1>
+  <p> ${req.body.message}</p>
+  `
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for port 465, false for other ports
+    auth: {
+      user: "azizozkaracadaag@gmail.com",
+      pass: "ntzgmpqqfhihzyfx",
+    },
+  });
+
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: 'Smart EDU Contact Form <azizozkaracadaag@gmail.com>', // sender address
+      to: "azizozkaracadagg@gmail.com", // list of receivers
+      subject: "Smart EDU Contact Form New Message", // Subject line
+      html: outputMessage, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  }
+
+  req.flash("Success", "We received your message successfully");
+
+  res.status(200).redirect("/contact");
+  
+}catch(error) {
+  req.flash("Error", error);
+  res.status(200).redirect("/contact");
+}
+}
+
+export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getContactPage, sendEmail };
